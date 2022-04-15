adec79cf85af55c9c27dd71568e14f37d8d164d9
Gregory DEPUILLE <gregory.depuille@sogelink.fr>
2022-04-15 11:41:46 +0200

    Shellcheck compliant

--- build-pi.sh
+++ build-pi.sh
@@ -9,22 +9,22 @@ ROOT_DIR=$(pwd)
 BUILD_NAME=build-pi
 BUILD_DIR=${ROOT_DIR}/${BUILD_NAME}
 
-cd ${ROOT_DIR}
+cd "${ROOT_DIR}" || exit
 if [ -d "${BUILD_DIR}" ] ; then
     echo "-- Nettoyage du répertoire de build ${BUILD_DIR}"
-    rm -Rf ${BUILD_DIR}
+    rm -Rf "${BUILD_DIR}"
 fi
 
 echo "-- Création du répertoire de build ${BUILD_DIR}"
-mkdir -p ${BUILD_DIR}
+mkdir -p "${BUILD_DIR}"
 
 sh download.sh ${BUILD_NAME}
 
 echo "-- Build OpenCV"
-cd ${ROOT_DIR}/download/opencv/${BUILD_NAME}
+cd "${ROOT_DIR}/download/opencv/${BUILD_NAME}" || exit
 cmake --build . || exit $?
 
 echo "-- Build du projet ${PROJECT}"
-cd ${BUILD_DIR}
+cd "${BUILD_DIR}" || exit
 cmake -DBUILD_PI=true .. || exit $?
 cmake --build . || exit $?
